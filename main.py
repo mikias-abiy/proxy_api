3d9764c72720217f25310aab2a66f854532edeac
Mikias <mikias_abiy@proton.me>
2024-04-09 19:12:21 +0300

    add: main.py: program entry file

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Standard modules and packages
+import logging
+
+# Local modules and packages
+from api.bot import bot
+import command_handlers
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    bot.polling()
