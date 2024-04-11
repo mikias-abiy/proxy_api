84572386b925725ec5f5f22e4d850a6f264ac9d3
Mikias <mikias_abiy@proton.me>
2024-04-11 14:33:01 +0300

    update: db_models/order.py: updated order_id columon to user the order key from the api instead of local key

--- models/db_models/order.py
+++ models/db_models/order.py
@@ -6,10 +6,9 @@ order.py:
 """
 
 from datetime import datetime
-import uuid
 
 from storm.locals import DateTime,\
-    Int, Unicode, UUID
+    Int, Unicode
 
 from models import storage
 
@@ -21,7 +20,7 @@ class Order:
     """
     __storm_table__ = 'orders'
 
-    order_id = UUID(primary=True)
+    order_id = Int(primary=True)
     created_at = DateTime()
     updated_at = DateTime()
     user_id = Int()
@@ -31,15 +30,15 @@ class Order:
     rental_period = Unicode(allow_none=True)
     item_delivered = Unicode(allow_none=True)
 
-    def __init__(self, user_id, **kwargs):
-        self.order_id = uuid.uuid4()
+    def __init__(self, order_id, user_id, amount_paid, rental_period, **kwargs):
+        self.order_id = order_id
         self.created_at = datetime.now()
         self.updated_at = self.created_at
         self.user_id = user_id
-        self.provider = kwargs.get('provider', "")
-        self.provider_id = kwargs.get('first_name', "")
-        self.amount_paid = kwargs.get('amount_paid', None)
-        self.rental_period = kwargs.get('rental_period', None)
+        self.amount_paid = amount_paid
+        self.rental_period = rental_period
+        self.provider = kwargs.get('provider', None)
+        self.provider_id = kwargs.get('provider_id', None)
         self.item_delivered = kwargs.get('item_delivered', None)
         storage.new(self)
 
