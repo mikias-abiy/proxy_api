0496225230958949c6e024c0577e3c911924fa54
Mikias <mikias_abiy@proton.me>
2024-04-16 03:49:24 +0300

    update: load_country_period_info.py: made adaptation to the new db model changes

--- load_country_period.py
+++ load_country_period.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
 
+import os
+import json
+
 from api.ps_api import ps_api
-from models.db_models.country import Country
-from models.db_models.period import Period
+from models import storage
+from models.db_models.proxy_type import ProxyType
+
+
+#!/usr/bin/python3
+
+from api.ps_api import ps_api
+from models.db_models.proxy_type import ProxyType
 
 from models import storage
 
+types = ['ipv4', 'isp', 'mobile']
 
-country_ipv4 = ps_api.referenceList('ipv4')['items']['country']
+for type in types:
+    response = ps_api.referenceList(type)['items']    
+    countries = response['country']
+    periods = response['period']
 
-country_mix = ps_api.referenceList('mix')['items']['country']
+    ProxyType(type, countries=countries, plans=None, periods=periods)
 
-periods = ps_api.referenceList('mix')['items']['period']
+response = ps_api.referenceList('resident')['items']    
 
+plans = response['tarifs']
 
-for country in country_ipv4:
-    name = country['name']
-    country_id = country['id']
-    type = 'ipv4'
-    cnt = Country(country_id=country_id, name=name, type=type)
-    cnt.save()
+with open('geo.zip', 'wb') as f:
+    f.write(b)
+os.system('unzip geo.zip')
+with open('geo.json', 'r') as f:
+    countries = f.read()
 
+ProxyType('resident', countries=countries, plans=plans, periods=None)
 
+# r_type = storage.store.get(ProxyType, 'resident')
 
-for country in country_mix:
-    name = country['name']
-    country_id = country['id']
-    type = 'ipv4'
-    cnt = Country(country_id=country_id, name=name, type=type)
-    cnt.save()
+# # b = ps_api.residentGeo()
 
-for period in periods:
-    response = ps_api.orderCalcIpv4(countryId=country_ipv4[0]['id'], periodId=period['id'], quantity=1, authorization=None, coupon=None, customTargetName="test_pack")
-    name = period['name']
-    period_id = period['id']
-    price = response['price']
-    prd = Period(period_id=period_id, name=name, price=price)
-    prd.save()
+# # 
+# # 
+# # 
+# # 
+# # 
+
+# print(json.dumps(r_type.countries, indent=2))
+
+m_type = storage.store.get(ProxyType, 'mobile')
+print(json.dumps(m_type.countries, indent=2))
